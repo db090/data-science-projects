e17f9ba0f26810817c354b696b8df8b6f98c3262
daniyal <dshamraiz61@gmail.com>
2025-03-27 19:48:50 +0500

    made some modifiactions

--- task-reminder/main.py
+++ task-reminder/main.py
@@ -1,20 +1,25 @@
 import streamlit as st
 import datetime
-import time
+
+# Set title and image
 st.title("Task Reminder App")
-st.image("https://cdn.ecommercedns.uk/files/8/212198/7/40437937/image.jpg",use_column_width=True)
-task=st.text_input("Enter the task you want to be reminded of:")
-reminder_duration=st.number_input("Set reminder duration (in minutes):",min_value=1,max_value=120)
+st.image("https://cdn.ecommercedns.uk/files/8/212198/7/40437937/image.jpg", use_column_width=True)
+
+# User input for task and reminder duration
+task = st.text_input("Enter the task you want to be reminded of:")
+reminder_duration = st.number_input("Set reminder duration (in minutes):", min_value=1, max_value=120)
+
+# Store reminder time in session state
 if st.button("Set Reminder"):
     if task and reminder_duration:
-        reminder_time=datetime.datetime.now()+datetime.timedelta(minutes=reminder_duration)
-        st.success(f"Remainder set for '{task}' in {reminder_duration} minutes at {reminder_time.strftime('%I:%M %p')}!")
-        while True:
-            current_time=datetime.datetime.now()
-            if current_time >= reminder_time:
-                st.balloons()
-                st.error(f"Time to: {task}!")
-                break
-            time.sleep(60)
-    else:
-        st.warning("Please enter a task and set a reminder duration!")
+        st.session_state.reminder_time = datetime.datetime.now() + datetime.timedelta(minutes=reminder_duration)
+        st.session_state.task = task
+        st.success(f"Reminder set for '{task}' in {reminder_duration} minutes!")
+
+# Check if reminder time has passed
+if "reminder_time" in st.session_state and "task" in st.session_state:
+    current_time = datetime.datetime.now()
+    if current_time >= st.session_state.reminder_time:
+        st.balloons()
+        st.error(f"Time to: {st.session_state.task}!")
+        del st.session_state.reminder_time  # Clear reminder after notification
